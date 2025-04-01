8e106ed2f113db19aff51ec4ba46dc007b69abad
LGY925 <olo66499133@gmail.com>
2025-04-01 16:13:08 +0900

    맵 만들다 맘

---
+++ Games/Game10/Program/Scenes/Town.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Game10.Util;
+
+namespace Game10.Scenes
+{
+    public class Town : Scene
+    {
+        public override void Render()
+        {
+            Console.WriteLine("당신은 "
+        }
+        public override void Choice()
+        {
+            
+        }
+        public override void Result(in int sieneNo)
+        {
+
+        }
+        public override void Wait()
+        {
+
+        }
+        public override void Next(in int sieneNo)
+        {
+            switch (sieneNo)
+            {
+                case (int)SieneNo.No1:
+                    
+                    break;
+                case (int)SieneNo.No3:
+                    Game.EndSwich();
+                    break;
+
+            }
+        }
+    }
+}
